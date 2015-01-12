9dbf72f7c8b81a16993475f1529a65b828a77ae1
shubhabrataroy <mail2shubho@gmail.com>
2015-01-12 17:39:51 +0100

    Create NYBikers.py

---
+++ NYBikers.py
@@ -0,0 +1,102 @@
+import time
+from dateutil.parser import parse
+import collections
+import sqlite3 as lite
+import requests
+from pandas.io.json import json_normalize
+import pandas as pd
+
+r = requests.get('http://www.citibikenyc.com/stations/json')
+key_list = [] #unique list of keys for each station listing
+for station in r.json()['stationBeanList']:
+    for k in station.keys():
+        if k not in key_list:
+            key_list.append(k)
+           
+
+df = json_normalize(r.json()['stationBeanList'])
+
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+with con:
+    cur.execute('CREATE TABLE citibike_reference (id INT PRIMARY KEY, totalDocks INT, city TEXT, altitude INT, stAddress2 TEXT, longitude NUMERIC, postalCode TEXT, testStation TEXT, stAddress1 TEXT, stationName TEXT, landMark TEXT, latitude NUMERIC, location TEXT )')
+
+sql = "INSERT INTO citibike_reference (id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)"
+
+#for loop to populate values in the database
+with con:
+    for station in r.json()['stationBeanList']:
+        #id, totalDocks, city, altitude, stAddress2, longitude, postalCode, testStation, stAddress1, stationName, landMark, latitude, location)
+        cur.execute(sql,(station['id'],station['totalDocks'],station['city'],station['altitude'],station['stAddress2'],station['longitude'],station['postalCode'],station['testStation'],station['stAddress1'],station['stationName'],station['landMark'],station['latitude'],station['location']))      
+
+station_ids = df['id'].tolist() 
+
+#add the '_' to the station name and also add the data type for SQLite
+station_ids = ['_' + str(x) + ' INT' for x in station_ids]
+
+#create the table
+#in this case, we're concatentating the string and joining all the station ids (now with '_' and 'INT' added)
+with con:
+    cur.execute("CREATE TABLE available_bikes ( execution_time INT, " +  ", ".join(station_ids) + ");")
+#take the string and parse it into a Python datetime object
+exec_time = parse(r.json()['executionTime'])
+with con:
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
+id_bikes = collections.defaultdict(int) #defaultdict to store available bikes by station
+
+#loop through the stations in the station list
+for station in r.json()['stationBeanList']:
+    id_bikes[station['id']] = station['availableBikes']
+
+#iterate through the defaultdict to update the values in the database
+with con:
+    for k, v in id_bikes.iteritems():
+        cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+
+for i in range(5):
+    r = requests.get('http://www.citibikenyc.com/stations/json')
+    exec_time = parse(r.json()['executionTime'])
+
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%s'),))
+    con.commit()
+
+    id_bikes = collections.defaultdict(int)
+    for station in r.json()['stationBeanList']:
+        id_bikes[station['id']] = station['availableBikes']
+
+    for k, v in id_bikes.iteritems():
+        cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = " + exec_time.strftime('%s') + ";")
+    con.commit()
+
+    time.sleep(60)
+
+con.close() #close the database connection when done
+
+### Analysis
+con = lite.connect('citi_bike.db')
+cur = con.cursor()
+df = pd.read_sql("SELECT * FROM available_bikes ORDER BY execution_time",con,index_col='execution_time')
+
+hour_change = collections.defaultdict(int)
+for col in df.columns:
+    station_vals = df[col].tolist()
+    station_id = col[1:] #trim the "_"
+    station_change = 0
+    for k,v in enumerate(station_vals):
+        if k < len(station_vals) - 1:
+            station_change += abs(station_vals[k] - station_vals[k+1])
+    hour_change[int(station_id)] = station_change #convert the station id back to integer
+    
+def keywithmaxval(d):
+    # create a list of the dict's keys and values; 
+    v = list(d.values())
+    k = list(d.keys())
+
+    # return the key with the max value
+    return k[v.index(max(v))]
+
+# assign the max key to max_station
+max_station = keywithmaxval(hour_change)
