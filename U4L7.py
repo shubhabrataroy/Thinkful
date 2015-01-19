85f128fa17c801fba802f16dccfc0245173d000f
shubhabrataroy <mail2shubho@gmail.com>
2015-01-19 17:18:14 +0100

    Create U4L7.py

---
+++ U4L7.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 19 17:08:38 2015
+
+@author: sroy
+"""
+from sklearn import decomposition
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
+
+iris = datasets.load_iris()
+X = iris.data
+y = iris.target
+
+pca = decomposition.PCA(n_components=3)
+pca.fit(X)
+X = pca.transform(X)
+
+neigh = KNeighborsClassifier(n_neighbors=6)
+neigh.fit(X,y)
+neigh.predict([[1.1]])
