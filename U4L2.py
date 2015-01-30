43f61eeeb178aede2d82ffef0e99337587e3011a
shubhabrataroy <mail2shubho@gmail.com>
2015-01-30 17:17:18 +0100

    Update U4L2.py

--- U4L2.py
+++ U4L2.py
@@ -8,3 +8,4 @@ Created on Fri Jan 30 16:51:10 2015
 import pandas as pd
 
 data1 = pd.read_csv('/home/sroy/Desktop/Thinkful/UCI HAR Dataset/features.txt', sep=' ',  header= None)
+# w['female'] = w['female'].map({'female': 1, 'male': 0})
