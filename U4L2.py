47325c466833f0cb2f203e4a2ea616919359f78f
shubhabrataroy <mail2shubho@gmail.com>
2015-01-30 17:05:49 +0100

    Create U4L2.py

---
+++ U4L2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 30 16:51:10 2015
+
+@author: sroy
+"""
+## Work in progress
+import pandas as pd
+
+data1 = pd.read_csv('/home/sroy/Desktop/Thinkful/UCI HAR Dataset/features.txt', sep=' ',  header= None)
