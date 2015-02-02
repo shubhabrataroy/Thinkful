314c2bdae75d999de674b1a7cb5949c17640623e
shubhabrataroy <mail2shubho@gmail.com>
2015-02-02 01:18:01 +0100

    Update U3L3.py

--- U3L3.py
+++ U3L3.py
@@ -6,15 +6,24 @@ Created on Tue Jan 27 19:23:41 2015
 """
 from bs4 import BeautifulSoup
 import requests
-import re
+import pandas as pd
 
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content)
-for row in soup('table'):
-    print row
-    
-for tag in soup.find_all(re.compile("^t")): # find all tag start with t
-    print tag.name
-    
+A = soup('table')[6].findAll('tr', {'class': 'tcont'})
+B = [x for x in A if len(x)== 25] # removing records without value
+
+records = []
+for rows in B:
+    col = rows.findAll('td')
+    country = col[0].string
+    year = col[1].string
+    total = col[4].string
+    men = col[7].string
+    women = col[10].string
+    record = (country, year, total, men, women)
+    records.append(record)
+column_name = ['country', 'year', 'total', 'men', 'women']
+table = pd.DataFrame(records, columns = column_name )
