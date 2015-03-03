81af60b692451cfca55a42da1140236c54ae5595
shubhabrataroy <mail2shubho@gmail.com>
2015-03-03 13:37:11 +0100

    Create density_datadict.py

---
+++ density_datadict.py
@@ -0,0 +1,17 @@
+import collections
+population_dict = collections.defaultdict(int)
+area_dict = collections.defaultdict(int)
+density_dict = collections.defaultdict(int)
+
+with open('/home/sroy/Desktop/Thinkful/lecz-urban-rural-population-land-area-estimates-v2-csv/lecz-urban-rural-population-land-area-estimates_continent-90m.csv','rU') as inputFile:
+    header = next(inputFile)
+
+    for line in inputFile:
+        line = line.rstrip().split(',')
+        line[5] = int(line[5])
+        line[7] = int(line[7])
+        if line[1] == 'Total National Population':
+            population_dict[line[0]] += line[5]
+            area_dict[line[0]] += line[7]
+for j in range(len(population_dict.keys())):
+    density_dict.update({population_dict.keys()[j]:float(population_dict[population_dict.keys()[j]]/area_dict[population_dict.keys()[j]])})
