de40a35ee48453d72ee398468ba447ca6e8621ac
shubhabrataroy <mail2shubho@gmail.com>
2015-03-06 02:38:03 +0100

    Update ChrisProject1.py

--- ChrisProject1.py
+++ ChrisProject1.py
@@ -20,3 +20,20 @@ for j in SC['Location 1']:
 
 SC['location'] = location
 
+#####################################################################################
+
+import pandas as pd
+CS = pd.io.excel.read_excel('/Users/shubhabrataroy/Desktop/Thinkful/Data/CrimeStatistics.xlt', sheetname=0)
+
+def getSec(s):
+    l = s.split(':')
+    return int(l[0]) * 3600 + int(l[1]) * 60 + int(l[2])
+seconds = []
+for i in range(len(CS)):
+    if ((':' in str(CS['CrimeTime'][i])) == True):
+        s = getSec(str(CS['CrimeTime'][i]))
+    else:
+        s = CS['CrimeTime'][i]
+    seconds.append(s)
+CS['CrimeTimeinSeconds'] = seconds
+
