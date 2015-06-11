3d8e3d51f3ba591dedd5a168622b2e52c83ad0cc
shubhabrataroy <mail2shubho@gmail.com>
2015-06-11 18:05:12 +0200

    Update U3L1.py

--- U3L1.py
+++ U3L1.py
@@ -1,7 +1,36 @@
-import pandas as pd
+
+# New Yorkers Bike
+import time
+import requests
+from dateutil.parser import parse
+import collections
 import sqlite3 as lite
+import pandas as pd
 import matplotlib.pyplot as plt
 import collections
+
+con = lite.connect('SQLite Data\citi_bike.sqlite')
+cur = con.cursor()
+
+for i in range(60):
+    r = requests.get('http://www.citibikenyc.com/stations/json')
+    exec_time = parse(r.json()['executionTime'])
+
+    cur.execute('INSERT INTO available_bikes (execution_time) VALUES (?)', (exec_time.strftime('%c'),))
+    con.commit()
+    
+    id_bikes = collections.defaultdict(int)
+    for station in r.json()['stationBeanList']:
+        id_bikes[station['id']] = station['availableBikes']
+
+    for k, v in id_bikes.iteritems():
+        cur.execute("UPDATE available_bikes SET _" + str(k) + " = " + str(v) + " WHERE execution_time = '" + exec_time.strftime('%c') + "';")
+    con.commit()
+
+    time.sleep(60)
+con.close()
+
+### Analysis
 
 con = lite.connect('SQLite Data\citi_bike.sqlite')
 cur = con.cursor()
